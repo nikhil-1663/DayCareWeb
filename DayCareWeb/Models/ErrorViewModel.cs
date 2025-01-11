13b30c7a556f0c6dda6c99de536df04a8b07f5a3
ST10084600 <ST10084600@vcconnect.edu.za>
2025-01-11 11:25:41 +0200

    Add project files.

---
+++ DayCareWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DayCareWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
